49e692ba4d34136c3f05531653c29524ed84fb27
Fazalwahab12 <wahabdir567@gmail.com>
2024-05-27 03:33:16 -0700

    first commit

---
+++ src/app/componts/Blog.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section>
+      <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+        <h1 className=" text-6xl text-black font-extrabold">
+          WE’RE NOT JUST FOR DATING
+        </h1>
+        <div className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
+          <div className="p-6">
+            <img
+              className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+              src="https://via.placeholder.com/150"
+              alt="blog"
+            />
+            <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+              Short headline.
+            </h1>
+            <p className="mx-auto text-base leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>{" "}
+          </div>
+          <div className="p-6">
+            <img
+              className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+              src="https://via.placeholder.com/150"
+              alt="blog"
+            />
+            <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+              Short headline.
+            </h1>
+            <p className="mx-auto text-base leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>{" "}
+          </div>
+          <div className="p-6">
+            <img
+              className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+              src="https://via.placeholder.com/150"
+              alt="blog"
+            />
+            <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+              Short headline.
+            </h1>
+            <p className="mx-auto text-base leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>{" "}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
---
+++ src/app/componts/Hero2.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const Hero2 = () => {
+  return (
+    <div
+      className="flex pt-12 px-6 md:px-20 items-center justify-center bg-hero md:h-screen overflow-hidden"
+      style={{ backgroundColor: "#FFC629" }}
+    >
+      <div className="flex flex-col gap-6 md:flex-row items-center max-w-8xl">
+        <div className="w-full md:w-1/2 lg:pr-32">
+          <h2 className="text-4xl lg:text-5xl text-center md:text-left font-extrabold text-black leading-tight">
+            There’s a better way to talk with your customers.
+          </h2>
+          <h3 className="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-left text-gray-700 font-light tracking-wider leading-relaxed">
+            Help Scout is designed with your customers in mind. Provide email
+            and live chat with a personal touch, and deliver help content right
+            where your customers need it, all in one place, all for one low
+            price.
+          </h3>
+          <div className="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
+            <button className="w-full sm:w-40 px-4 py-3 rounded font-semibold text-md  bg-black text-white ">
+              Get started
+            </button>
+          </div>
+        </div>
+        <div className="w-full p-16   md:w-1/2 flex justify-center md:justify-end">
+          <img
+            src="https://bumble.com/_next/image?url=%2Fbumble-brand-assets%2Flanding%2Fassets%2Fphotos%2Flanding-section-1.jpg&w=561&q=75"
+            className="  rounded-2xl "
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero2;
